05b0a01ab7bdf18b21c363762712ccf923357d93
JOjo <jolinlin.tan@hotmail.com>
2025-07-28 17:34:49 +0800

    updated

--- javascript.js
+++ javascript.js
@@ -1,81 +1,166 @@
-//Buttons:
-    const wingBtn = document.getElementById("wingBtn");
-    const headBtn = document.getElementById("headBtn");
+//get main menu page:
+const mainMenu = document.getElementById("mainPage");
 
-//Windows:
-    const wingWindow = document.getElementById("wingWindow");
-    const headWindow = document.getElementById("headWindow");
+//select all the circles
+document.querySelectorAll(".circle").forEach(a => {
+    const circle = a.dataset.part;
+    a.addEventListener("click", () => openWindow(circle));
+})
 
-//Pigeon parts:
-    const pigeonWing = document.getElementById("pigeonWing");
-    const headWing = document.getElementById("pigeonHead");
-
-//WING WINDOW:
-    function closeWingWindow() {
-            wingWindow.classList.remove("windowAnimationOpen");
-            wingWindow.classList.add("windowAnimationClose");
+//select all the close buttons
+document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn"))
+    {
+        const pigPart = event.target.dataset.part;
+        closeWindow(pigPart);
     }
-    wingBtn.addEventListener("click",closeWingWindow);
-
-    function clickWingWindow() {
-        wingWindow.classList.remove("hiddenItem");
-        wingWindow.classList.add("windowAnimationOpen");
-    }
-    pigeonWing.addEventListener("click",clickWingWindow);
-
-    wingWindow.addEventListener('animationend', (e) => {
-        if (e.animationName == "windowAnimationFramesClose")
-        {
-            wingWindow.classList.add("hiddenItem");
-            wingWindow.classList.remove("windowAnimationClose");
-            wingWindow.classList.remove("windowAnimationOpen");
-        }
-    });
-
-//HEAD WINDOW
-    function closeHeadWindow() {
-            headWindow.classList.add("windowAnimationClose");
-    }
-    headBtn.addEventListener("click",closeHeadWindow);
-
-    function clickHeadWindow() {
-        headWindow.classList.remove("hiddenItem");
-        headWindow.classList.add("windowAnimationOpen");
-    }
-    pigeonHead.addEventListener("click",clickHeadWindow);
-
-    headWindow.addEventListener('animationend', (e) => {
-        if (e.animationName == "windowAnimationFramesClose")
-        {
-            headWindow.classList.add("hiddenItem");
-            headWindow.classList.remove("windowAnimationClose");
-            headWindow.classList.remove("windowAnimationOpen");
-        }
-    });
-
-//BOUNCING PIGEON
-const pigeonID = document.getElementById("bouncePigeon");
-
-function clickPigeon() {
-    pigeonID.classList.add("bouncingItem");
-}
-
-pigeonID.addEventListener("click",clickPigeon);
-pigeonID.addEventListener('animationend', () => {
-    pigeonID.classList.remove("bouncingItem");
 });
 
-// //LOCKED SCROLLING:
-// const lockedScroll = document.getElementById("lockedScroll");
-// const lockedScrollContent = document.querySelector(".lockedScroll");
+//open window for pigeon body parts:
+function openWindow(circle) {
+    const circlebtn = document.getElementById(`${circle}Window`);
+    circlebtn.classList.remove("hiddenItem");
+    circlebtn.classList.remove("windowAnimationClose");
+    circlebtn.classList.add("windowAnimationOpen");
+}
+//close window for pigeon body parts:
+function closeWindow(button) {
+    const win = document.getElementById(`${button}Window`);
 
-// function toggleScroll(check) {
-//     document.body.style.overflow = check ? "hidden" : "auto";
-// }
+    if(win) //if it exists:
+    {
+        win.classList.remove("windowAnimationOpen");
+        win.classList.add("windowAnimationClose");
+        
+        win.addEventListener('animationend', () => {
+            win.classList.add("hiddenItem");
+            win.classList.remove("windowAnimationClose");
+        })
+    }
+}
 
-// toggleScroll(true);
+document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn"))
+    {
+        const pigPart = event.target.dataset.part;
+        closeWindow(pigPart);
+    }
+});
 
-// lockedScrollContent.addEventListener('scroll', () => {
-//     const hitBtm =  lockedScrollContent.scrollTop +
+//BOUNCING PIGEON + menu
+const pigeonID = document.getElementById("hamMenuBtnID");
+const menuID = document.getElementById("menu")
 
-// });
+function clickPigeon() 
+{
+    if (pigeonID.classList.contains("slidedown")) {
+        pigeonID.classList.remove("slidedown");
+        pigeonID.classList.add("slideup");
+    }
+    else {
+        pigeonID.classList.remove("slideup");
+        pigeonID.classList.add("slidedown");
+    }
+
+    if (menuID.classList.contains("menuAnimOpen")) {
+        menuID.classList.remove("menuAnimOpen");
+        menuID.classList.add("menuAnimClose");
+    }
+    else {
+        menuID.classList.remove("menuAnimClose");
+        menuID.classList.add("menuAnimOpen");
+    }
+}
+pigeonID.addEventListener("click", clickPigeon);
+
+let sectionElement = document.getElementById('pigeonHistoryContent');
+let buttonElement = document.getElementById('pigeonHistoryID');
+
+document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("hamMenuButtons"))
+    {
+        const hamBtn = event.target.dataset.contentbtns;
+        displayHistory();
+    }
+});
+
+const pigeonHistoryContent = document.getElementById("pigeonHistoryContent");
+const pigeonHistoryBtn = document.getElementById("pigeonHistoryID");
+
+function displayHistory() 
+{
+    mainMenu.style.display = "block";
+    mainMenu.classList.add("hideAndBlur");
+    
+    //scroll to the very top of the content page
+    pigeonHistoryContent.scrollTo({top: 0, behavior: "smooth"});
+}
+
+
+document.body.addEventListener("click", (event) => {
+    if (event.target.id == "backBtn")
+    {
+        pigeonHistoryContent.classList.remove("unHideAndUnBlur");
+        pigeonHistoryContent.classList.add("hideAndBlur");
+
+        pigeonHistoryContent.addEventListener('animationend', (e) => {
+            const animationName = e.animationName;
+            //console.log(`Animation "${animationName}" has ended.`);
+            if (animationName == "hideAndBlurFrames")
+            {
+                mainMenu.style.display = "block";
+                mainMenu.classList.remove("hideMenu");
+        
+                pigeonHistoryContent.classList.remove("hideAndBlur");
+                pigeonHistoryContent.classList.add("hiddenItem");
+            }
+        })
+    }
+});
+
+
+
+pigeonHistoryBtn.addEventListener("click", displayHistory);
+
+mainMenu.addEventListener("animationend", () => {
+        mainMenu.style.display = "none";
+        mainMenu.classList.remove("hideAndBlur");
+
+        pigeonHistoryContent.classList.add("unHideAndUnBlur");
+        pigeonHistoryContent.classList.remove("hideAndBlur");
+        pigeonHistoryContent.classList.remove("hiddenItem");
+})
+
+document.body.addEventListener("click", (event) => {
+    if (event.target.id == "backBtn")
+    {
+        pigeonHistoryContent.classList.remove("unHideAndUnBlur");
+        pigeonHistoryContent.classList.add("hideAndBlur");
+
+        pigeonHistoryContent.addEventListener('animationend', (e) => {
+            const animationName = e.animationName;
+            //console.log(`Animation "${animationName}" has ended.`);
+            if (animationName == "hideAndBlurFrames")
+            {
+                mainMenu.style.display = "block";
+                mainMenu.classList.remove("hideMenu");
+        
+                pigeonHistoryContent.classList.remove("hideAndBlur");
+                pigeonHistoryContent.classList.add("hiddenItem");
+            }
+        })
+    }
+});
+////////////////////
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if (entry.isIntersecting)
+            entry.target.classList.add("show");
+        else
+            entry.target.classList.remove("show"); })
+}, { threshold:.2 } ); 
+
+const hiddenElements = document.querySelectorAll(".hide");
+hiddenElements.forEach(element => {
+    observer.observe(element);
+});
