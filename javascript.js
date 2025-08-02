dc3a350797b7cfad129f1b6ba4517d6e3435809e
JOjo <jolinlin.tan@hotmail.com>
2025-08-03 02:17:34 +0800

    added manifest.json and updated files for mobile app

--- javascript.js
+++ javascript.js
@@ -692,3 +692,31 @@ document.addEventListener("mousemove", (e) =>{
     interactableObj.style.left = x + "px";
     interactableObj.style.top = y + "px";
 });
+
+//fullscreen:
+const btnFS=document.querySelector("#btnFS");
+const btnWS=document.querySelector("#btnWS");
+btnFS.addEventListener("click",enterFullscreen);
+btnWS.addEventListener("click",exitFullscreen);
+function enterFullscreen() { //must be called by user generated event
+if (document.documentElement.requestFullscreen) {
+document.documentElement.requestFullscreen();
+} else if (document.documentElement.mozRequestFullScreen) { // Firefox
+document.documentElement.mozRequestFullScreen();
+} else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+document.documentElement.webkitRequestFullscreen();
+} else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+document.documentElement.msRequestFullscreen();
+}
+}
+function exitFullscreen() {
+if (document.exitFullscreen) {
+document.exitFullscreen();
+} else if (document.mozCancelFullScreen) { // Firefox
+document.mozCancelFullScreen();
+} else if (document.webkitExitFullscreen) { // Chrome, Safari, and Opera
+document.webkitExitFullscreen();
+} else if (document.msExitFullscreen) { // IE/Edge
+document.msExitFullscreen();
+}
+}
